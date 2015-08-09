7d69e1c09ba2406e60abdbf5b39804c63eeb09c3
arya45 <audrey.dogbeh@gmail.com>
2015-08-09 19:22:38 +0100

    Bonfire: Search and Replace

---
+++ search_replace.js
@@ -0,0 +1,13 @@
+function replace(str, before, after) {
+  
+  if (before[0]===before[0].toUpperCase()){
+    after=after[0].toUpperCase()+after.slice(1);
+  } else {
+    after=after[0].toLowerCase()+after.slice(1);
+  }
+  str=str.replace(before,after);
+  
+  return str;
+}
+
+replace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
